d2f70f0301c9fd1db37ae3b51b6c70aa9ff8ca9a
Alejandro Bryan <alejandrobryan023@gmail.com>
2022-10-14 12:21:06 +0200

    test

---
+++ docs/contact.50b85530.js
@@ -0,0 +1,2 @@
+let e=document.querySelector(".wappe");console.log(e);
+//# sourceMappingURL=contact.50b85530.js.map
--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,4 @@
+import '../scss/styles.scss';
+
 let wappe = document.querySelector('.wappe');
 console.log(wappe);
